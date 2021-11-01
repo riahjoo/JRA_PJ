a047dc619c09a0af867385ae8d799b86474bbfe9
riahjoo <90014579+riahjoo@users.noreply.github.com>
2021-11-02 01:04:03 +0900

    .

--- 포폴/js/main.js
+++ 포폴/js/main.js
@@ -3,22 +3,28 @@
 
 $(function () {
 
-// main visual 슬라이드 
+    // main visual 슬라이드 
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+    var swiper = new Swiper(".mySwiper", {
+        
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        },
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        },
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+    });
+
 
 });
