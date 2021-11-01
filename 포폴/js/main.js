361857fcd50a9d57adbbcb11f6bb90ec8b684605
riahjoo <90014579+riahjoo@users.noreply.github.com>
2021-11-01 18:57:32 +0900

    .

---
+++ 포폴/js/main.js
@@ -0,0 +1,24 @@
+
+
+
+$(function () {
+
+// main visual 슬라이드 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+});
