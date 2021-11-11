b18157384236147d1d13176f4508cf09433b3a01
riahjoo <90014579+riahjoo@users.noreply.github.com>
2021-11-11 17:08:05 +0900

    .

--- gongcha/js/best.js
+++ gongcha/js/best.js
@@ -10,6 +10,8 @@ $(window).on('load', function () {
     let bestImgNum
     let bestWidth
     let timer
+    let bestOverNum = 0
+    let bestDot
 
     bestInit(); // 초기함수
     bestReset();
@@ -24,6 +26,7 @@ $(window).on('load', function () {
         bestBtnImg = $(".best_arrow img");
         bestImgNum = bestLi.length;
         bestList.children().last().prependTo(bestList);
+        bestDot = $(".best_b li");
     } // init 함수
   
     function bestReset() {
@@ -62,7 +65,24 @@ $(window).on('load', function () {
 
     } // bestBtnEvent 함수 : 마우스 이벤트
 
-    let Num=1;
+   
+    let Num=1;//종류 순번
+   
+
+    function  sortOn(bb) {
+
+        let pm = "종류"+bb; // 종류 문자 담는 변수
+        let tx = pm.toString(); // 문자화
+        let data = binfo[tx];
+        $(".best_topp img").attr("src", data["이미지"]);
+        $(".best_topp_sort").text(data["토핑"]);
+        $(".best_topp_priceL").text(data["large"]);
+        $(".best_topp_priceJ").text(data["jumbo"]);
+       
+
+    }// best_info 부분 내용 변경.
+
+   
 
     function onbestSlideNext() {
        
@@ -75,6 +95,11 @@ $(window).on('load', function () {
             Num++;
             if (Num > bestImgNum) Num = 1;
             sortOn(Num);
+
+            bestOverNum++;
+            if (bestOverNum === bestImgNum) bestOverNum = 0;
+            onBestShowDot(bestOverNum)
+
             bestList.children().first().appendTo(bestList)
             bestList.css({
                 left: -bestWidth * 2
@@ -84,32 +109,6 @@ $(window).on('load', function () {
       
 
     } // 왼쪽으로 애니메이션 (슬라이드) 
-
-    let pm; 
-    console.log(pm);
-
-    let data;
-
-    let bestToppImg = $(".best_topp img");
-
-    bestList.children("li").eq(2).css({
-
-        border: "2px solid blue"
-
-    })
-
-    
-
-    function  sortOn(bb) {
-
-        pm = "종류"+bb;
-        let tx = pm.toString();
-        data = sinfo[tx];
-        bestToppImg.attr("src", data["이미지"]);
-       
-
-    }// best_info 부분 내용 변경.
-
 
     function onbestSlidePrev() {
 
@@ -121,6 +120,11 @@ $(window).on('load', function () {
             Num--;
             if (Num === 0) Num = bestImgNum;
             sortOn(Num);
+
+            bestOverNum--;
+            if (bestOverNum === -1) bestOverNum = bestImgNum - 1;
+            onBestShowDot(bestOverNum);
+
             bestList.children().last().prependTo(bestList)
             bestList.css({
                 left: -bestWidth * 2
@@ -130,6 +134,13 @@ $(window).on('load', function () {
         })
 
     } // 오른쪽으로 에니메이션 (슬라이드) 
+
+
+    function onBestShowDot() {
+        bestDot.eq(bestOverNum).addClass("selected").siblings().removeClass("selected"); 
+        
+
+    } // bullet 보여주기
 
 
     function bestOnPlay() {
@@ -161,8 +172,8 @@ $(window).on('load', function () {
 
 
 
-    ///// 각 서브별 데이터 셋팅하기 /////
-    let sinfo = {
+    ///// 각 데이터 셋팅하기 /////
+    let binfo = {
         "종류1": {
             "이미지": "./images/best_topp.jpg",
             "토핑": "펄(타피오카)",
@@ -195,7 +206,7 @@ $(window).on('load', function () {
 
         },
 
-    }; ////////// sinfo ////////////////
+    }; ////////// best_info 부분 데이터 세팅 ////////////////
 
   
 })
---
+++ gongcha/js/mainVisual.js
@@ -0,0 +1,185 @@
+$(window).on('load', function () {
+
+    // main visual 슬라이드 
+
+    let visualInner
+    let visualList
+    let visualLi
+    let visualImg
+    let btnImg
+    let visualImgNum
+    let visualWidth
+    let timer
+    let dot
+    let visualOverNum = 0
+
+    init(); 
+    visualReset();
+    onShowDot(0);
+    onPlay();
+    inEvent();
+
+    function init() {
+        visualInner = $(".visual");
+        visualList = $(".visual_list");
+        visualLi = visualList.children()
+        visualImg = $(".visual_list li img");
+        btnImg = $(".visual_arrow img");
+        visualImgNum = visualLi.length;
+        visualList.children().last().prependTo(visualList);
+        dot = $(".bullet").children();
+
+    } // init 함수
+
+    function visualReset() {
+
+        visualWidth = visualInner.width();
+        visualLi.css({
+            width: visualWidth
+        });
+        visualImg.css({
+            width: visualWidth * 0.84
+        });
+
+        visualList.css({
+            width: visualWidth * visualImgNum,
+            left: -visualWidth * 2
+        });
+
+
+    } // visualReset 함수 : 초기 설정
+
+    function inEvent() {
+
+        $(window).on("resize", visualReset);
+        $(".right_arrow").on("click", onVisualSlideNext);
+        $(".left_arrow").on("click", onVisualSlidePrev);
+
+        visualImg.on("mouseleave", onPlay);
+        visualImg.on("mouseenter", onStop);
+        btnEvent();
+
+
+    } // inEvent 함수 
+
+
+    function btnEvent() {
+        btnImg.on("mouseenter", onStop)
+        btnImg.on("mouseleave", onPlay)
+        dot.on("mouseenter", onShowDot)
+        dot.on("mouseleave", onPlay)
+    
+    } // btnEvent 함수 : 마우스 이벤트
+
+
+
+    function onShowDot() {
+       
+        dot.eq(visualOverNum).addClass("selected").siblings().removeClass("selected"); // 선택된 dot 에만 활성화 배경색 적용 
+
+    } // bullet 보여주기
+
+
+
+    function onVisualSlideNext() {
+
+
+        let currentPosition = visualList.position().left
+
+        $(".visual_list:not(:animated)").animate({
+            left: currentPosition - visualWidth
+
+        }, 400, "easeOutCubic", function () {
+            visualOverNum++;
+            if (visualOverNum === visualImgNum) visualOverNum = 0;
+            onShowDot(visualOverNum)
+            visualList.children().first().appendTo(visualList)
+            visualList.css({
+                left: -visualWidth * 2
+            })
+
+        })
+
+    } // 왼쪽으로 애니메이션 (슬라이드) 
+
+
+    function onVisualSlidePrev() {
+
+        let currentPosition = visualList.position().left
+
+        $(".visual_list:not(:animated)").animate({
+            left: currentPosition + visualWidth
+        }, 400, "easeOutCubic", function () {
+            visualOverNum--;
+            if (visualOverNum === -1) visualOverNum = visualImgNum - 1;
+            onShowDot(visualOverNum)
+            visualList.children().last().prependTo(visualList)
+            visualList.css({
+                left: -visualWidth * 2
+            })
+
+        })
+
+    } // 오른쪽으로 에니메이션 (슬라이드) 
+
+    function onPlay() {
+
+        timer = setInterval(onVisualSlideNext, 3000)
+
+    }
+
+    function onStop() {
+
+        clearInterval(timer)
+
+    }
+
+
+    let swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        }
+
+    }); // swiper //
+
+
+});
+
+
+$(document).ready(function () {
+
+    ///////// 모바일 이미지로 변경(세로형)
+    if ($(window).innerWidth() < 750) {
+
+        mobileImage()
+
+    } // if문 (width : 750px이하 일때 함수 실행)
+
+
+    function mobileImage() {
+
+        let mi = $(".swiper-wrapper").children(); // mobile image
+        let ich; // image change
+
+        for (i = 0; i < 5; i++) {
+
+            ich = mi.eq(i).children();
+            console.log(i)
+            ich.attr("src", "images/top_Mimg0" + (i + 1) + ".gif");
+
+        } // for문
+
+
+
+    } /////// 모바일 이미지로 변경(세로형)함수
+
+
+})
