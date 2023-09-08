d1bf53f80c9f64fdf626be956fbc382bdd770b8f
RIAHJOO <90014579+riahjoo@users.noreply.github.com>
2023-09-08 11:33:17 +0900

    Update main.js
    ㄴㄴ

--- profile/js/main.js
+++ profile/js/main.js
@@ -10,4 +10,17 @@ $(window).on('load', function () {
       });
 
 
+    export default function preventZoom() {
+  function listener(event: TouchEvent) {
+    // 핀치 줌의 경우 두 개 이상의 이벤트가 발생한다.
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  }
+
+  document.addEventListener('touchmove', listener, { passive: false });
+}
+preventZoom();
+
+
 })
